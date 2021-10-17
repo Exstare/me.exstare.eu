c3a55afaaac1a5bd1e5b5305cf6174cc4023bc45
DieGoedeMan <68126277+Exstare@users.noreply.github.com>
2021-10-17 20:24:32 +0200

    Fixed projects (non-responsive)

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
     <div className="bg-background h-screen w-screen p-12">
       {/* Me card */}
       <div className="flex flex-row bg-secondary w-full content-center place-items-center p-10 rounded-card justify-between">
-        
+
         {/* Left */}
         <div className="flex items-center justify-center">
-        <img src="https://media.discordapp.net/attachments/869479978515959879/894546333170548746/image0_1.jpg" className="w-44 ml-5 rounded-2xl" alt="pf" />
+          <img src="https://media.discordapp.net/attachments/869479978515959879/894546333170548746/image0_1.jpg" className="w-44 ml-5 rounded-2xl" alt="pf" />
           <div className="text-white ml-7">
             <h1 className="font-bold text-3xl">Exstare</h1>
             <div className="text-lg">Professional idiot, gamer, coder</div>
@@ -32,16 +32,20 @@ function App() {
 
       {/* Projects */}
       <div className="mt-5 text-white">
-        <div className="text-2xl font-bold">Projects</div>
-        <div className="flex flex-row space-x-4 -2">
+        <div className="text-2xl font-bold mb-2">Projects</div>
+        <div className="flex space-x-4 -2">
           {
             projects.map(item => {
               return (
-                <div className="bg-secondary p-5 rounded-xl">
-                  <img src={item.img} alt={item.id} className="w-1/12" />
-                  <h2 className="text-xl font-bold">{item.name}</h2>
-                  <div className="">{item.desc}</div>
+
+                <div className="bg-secondary flex items-center justify-start p-5 rounded-xl">
+                  <img src={item.img} className="w-1/5 rounded-2xl" alt={item.id} />
+                  <div className="text-white ml-7">
+                    <h1 className="font-bold text-xl">{item.name}</h1>
+                    <div className="text-lg">{item.desc}</div>
+                  </div>
                 </div>
+
               )
             })
           }
