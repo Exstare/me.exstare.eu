b69b2d7a1c77a31f61e167a3e9d0767df230cc88
DieGoedeMan <68126277+Exstare@users.noreply.github.com>
2021-12-02 22:16:33 +0100

    Made it ready for V1

--- src/App.js
+++ src/App.js
@@ -1,66 +1,77 @@
 import dob from 'dob-to-age';
-import projects from './data/projects.json';
 import socials from './data/socials.json';
 
-import { FaGithub, FaYoutube, FaTwitch, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { FaGithub, FaYoutube, FaTwitch, FaInstagram, FaTwitter, FaExternalLinkAlt } from 'react-icons/fa';
 import { BsDiscord } from 'react-icons/bs';
 
-const icons = [FaGithub, BsDiscord, FaYoutube, FaTwitch, FaInstagram, FaTwitter]
+import stackImg from './data/stack.png';
+import profileImg from './data/pf.jpg'
+
+const icons = [FaGithub, BsDiscord, FaYoutube, FaTwitch, FaInstagram, FaTwitter];
+const age = dob("2004-03-25")
+const ageHref = `https://en.wikipedia.org/wiki/${age}_(number)`;
 
 function App() {
   return (
-    <div className="bg-background h-screen w-screen p-12">
+    <div className="bg-background h-screen w-screen p-10">
       {/* Me card */}
       <div className="flex flex-row bg-secondary w-full content-center place-items-center p-10 rounded-card justify-between">
-
         {/* Left */}
         <div className="flex items-center justify-center">
-          <img src="https://media.discordapp.net/attachments/869479978515959879/894546333170548746/image0_1.jpg" className="w-44 ml-5 rounded-2xl" alt="pf" />
+          <img src={profileImg} className="w-44 ml-5 rounded-2xl" alt="pf" />
           <div className="text-white ml-7">
             <h1 className="font-bold text-3xl">Exstare</h1>
-            <div className="text-lg">Professional idiot, gamer, coder</div>
+            <div className="text-lg italic">Professional idiot, gamer, coder, bug creator</div>
+            <div className="text-gray-400 text-sm">I know this site is not responsive, will be coming soon I hope o.O</div>
           </div>
         </div>
-
         {/* Right */}
-        <div className="text-white text-lg space-y-5 mr-16">
-          <div>👨 | He/him</div>
-          <div>🎂 | {dob('2004-03-25')}</div>
-          <div>🌍 | The Netherlands</div>
+        <div className="text-white text-lg space-y-5 mr-16 hover:animate-bounce">
+          <div><a href="https://pronoun.is/he" target="_blank" rel="noreferrer">👨 | He/him</a></div>
+          <div><a href={ageHref} target="_blank" rel="noreferrer">🎂 | {age}</a></div>
+          <div><a href="https://www.youtube.com/watch?v=ZXavgFHXj0g" target="_blank" rel="noreferrer">🌍 | The Netherlands</a></div>
         </div>
       </div>
-
-      {/* Projects */}
+      {/* Middle */}
       <div className="mt-5 text-white">
-        <div className="text-2xl font-bold mb-2">Projects</div>
-        <div className="flex space-x-4 -2">
-          {
-            projects.map(item => {
-              return (
-
-                <div className="bg-secondary flex items-center justify-start p-5 rounded-xl">
-                  <img src={item.img} className="w-1/5 rounded-2xl" alt={item.id} />
-                  <div className="text-white ml-7">
-                    <h1 className="font-bold text-xl">{item.name}</h1>
-                    <div className="text-lg">{item.desc}</div>
-                  </div>
-                </div>
-
-              )
-            })
-          }
+        <div className="grid grid-cols-3 space-x-5">
+          {/* Discord */}
+          <div className="bg-secondary flex justify-center rounded-xl">
+            <img src="https://lanyard-profile-readme.vercel.app/api/462914535351779328?bg=1D2331&idleMessage=Not%20doing%20shit%20probably :D" alt="profile" />
+          </div>
+          {/* Some projects */}
+          <div className="bg-secondary p-5 rounded-xl grid grid-rows-2">
+            <div>
+              <a href="https://myranks.netlify.app/" target="_blank" rel="noreferrer" className="flex flex-row items-center mt-2">
+                <h3 className="font-bold text-lg mr-2">MyRanks</h3>
+                <FaExternalLinkAlt />
+              </a>
+              <p className="">Share your game ranks with your personal link!</p>
+              <p className="text-gray-400 italic text-sm">WIP | Only front-end | Still needs a logo</p>
+            </div>
+            <div>
+              <a href="https://github.com/Exstare/KahootSpammer" target="_blank" rel="noreferrer" className="flex flex-row items-center">
+                <h3 className="font-bold text-lg mr-2">KahootSpammer</h3>
+                <FaExternalLinkAlt />
+              </a>
+              <p className="">Spam Kahoots with this little CLI tool!</p>
+              <p className="text-gray-400 italic text-sm">Update coming... Eventually</p>
+            </div>
+          </div>
+          {/* Stack */}
+          <div className="bg-secondary flex items-center justify-start p-5 rounded-xl">
+            <a href="https://stackshare.io/exstare/my-stack/main#stack" target="_blank" rel="noreferrer"><img src={stackImg} alt="stack" /></a>
+          </div>
         </div>
       </div>
-
-      {/* Socals */}
+      {/* Socials */}
       <div className="mt-5 text-white">
-        <div className="text-2xl font-bold">Socials</div>
         <div className="flex justify-between mt-2">
           {
             socials.map((item, idx) => {
               const Icon = icons[idx];
-              const iconClass = `h-14 w-14 fill-current text-icons-white`;
-              const bgClass = `flex rounded-xl h-24 w-24 flex items-center justify-center bg-icons-${item.id}`
+              const iconClass = `h-14 w-14 fill-current text-icons-white hover:animate-bounce`;
+              const bgClass = `flex rounded-xl h-24 w-40 flex items-center justify-center bg-icons-${item.id}`
 
               return (
                 <a href={item.link} target="_blank" rel="noreferrer" className={bgClass}>
@@ -71,7 +82,6 @@ function App() {
           }
         </div>
       </div>
-
     </div>
   );
 }
